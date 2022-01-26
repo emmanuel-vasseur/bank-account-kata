162d0b3bb2905ef36388d06e9136e5940227789c
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2022-01-26 23:23:33 +0100

    feat(make-a-deposit): refactor - use BigDecimal - failing test

--- src/test/java/com/sgbu/MakeADepositTest.java
+++ src/test/java/com/sgbu/MakeADepositTest.java
@@ -2,6 +2,8 @@ package com.sgbu;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MakeADepositTest {
@@ -9,13 +11,13 @@ class MakeADepositTest {
     @Test
     void create_an_account_should_initialize_balance_to_zero() {
         Account account = new Account();
-        assertThat(account.getBalance()).isEqualTo(0);
+        assertThat(account.getBalance()).isEqualTo(BigDecimal.ZERO);
     }
 
     @Test
     void make_a_deposit_should_add_amount_to_balance() {
         Account account = new Account();
-        account.deposit(200);
-        assertThat(account.getBalance()).isEqualTo(200);
+        account.deposit(new BigDecimal("201.01"));
+        assertThat(account.getBalance()).isEqualTo(new BigDecimal("201.01"));
     }
 }
