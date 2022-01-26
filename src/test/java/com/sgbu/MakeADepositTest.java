6dbf6eff160c1ec20c39f91ebd00ecca6b878541
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2022-01-26 23:22:12 +0100

    feat(make-a-deposit): refactor test

--- src/test/java/com/sgbu/MakeADepositTest.java
+++ src/test/java/com/sgbu/MakeADepositTest.java
@@ -7,9 +7,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MakeADepositTest {
 
     @Test
-    void make_a_deposit_add_amount_to_balance() {
+    void create_an_account_should_initialize_balance_to_zero() {
         Account account = new Account();
         assertThat(account.getBalance()).isEqualTo(0);
+    }
+
+    @Test
+    void make_a_deposit_should_add_amount_to_balance() {
+        Account account = new Account();
         account.deposit(200);
         assertThat(account.getBalance()).isEqualTo(200);
     }
