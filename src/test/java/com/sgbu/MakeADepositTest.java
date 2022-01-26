aecf56f70438a1aa497d8020eaa77cedbfabd109
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2022-01-26 23:20:30 +0100

    feat(make-a-deposit): failing test - loop 3

--- src/test/java/com/sgbu/MakeADepositTest.java
+++ src/test/java/com/sgbu/MakeADepositTest.java
@@ -10,5 +10,7 @@ class MakeADepositTest {
     void make_a_deposit_add_amount_to_balance() {
         Account account = new Account();
         assertThat(account.getBalance()).isEqualTo(0);
+        account.deposit(200);
+        assertThat(account.getBalance()).isEqualTo(200);
     }
 }
