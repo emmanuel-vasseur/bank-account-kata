0453abf76500ba802931460d43172533477c5e61
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2022-01-27 11:10:01 +0100

    feat(make-a-withdrawal): add simplest code to make the test pass for deposit or withdraw with zero or negative amount

--- src/main/java/com/sgbu/Account.java
+++ src/main/java/com/sgbu/Account.java
@@ -2,21 +2,32 @@ package com.sgbu;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.ZERO;
+
 public class Account {
-    private BigDecimal balance = BigDecimal.ZERO;
+    private BigDecimal balance = ZERO;
 
     public BigDecimal getBalance() {
         return this.balance;
     }
 
     public void deposit(BigDecimal amount) {
+        if(ZERO.compareTo(amount) >= 0) {
+            throw new RuntimeException("Invalid operation amount");
+        }
+
         this.balance = this.balance.add(amount);
     }
 
     public void withdrawal(BigDecimal amount) {
+        if(ZERO.compareTo(amount) >= 0) {
+            throw new RuntimeException("Invalid operation amount");
+        }
+
         if(amount.compareTo(balance) > 0) {
             throw new RuntimeException("Insufficient amount");
         }
+
         this.balance = this.balance.subtract(amount);
     }
 }
