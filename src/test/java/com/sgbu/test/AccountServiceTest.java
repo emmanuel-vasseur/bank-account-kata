f3d3dd85c4b8c38f2e253afc64ef10b9bd075e3f
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2022-01-27 09:21:48 +0100

    feat(make-a-deposit): refactor tests

---
+++ src/test/java/com/sgbu/test/AccountServiceTest.java
@@ -0,0 +1,22 @@
+package com.sgbu.test;
+
+import com.sgbu.AccountService;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class AccountServiceTest {
+
+    @Test
+    void make_a_deposit_for_an_non_existing_client_should_fail(){
+        AccountService accountService = new AccountService();
+        assertThatThrownBy(() -> accountService.makeDeposit(BigDecimal.TEN))
+                .hasMessage("Client not found");
+    }
+
+}
