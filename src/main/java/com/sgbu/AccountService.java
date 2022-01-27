5839c372a233ada835800140291f7e59aefdd6da
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2022-01-27 09:11:14 +0100

    feat(make-a-deposit): create AccountService - loop 1 - simplest code to make the test pass

---
+++ src/main/java/com/sgbu/AccountService.java
@@ -0,0 +1,11 @@
+package com.sgbu;
+
+import java.math.BigDecimal;
+
+public class AccountService {
+
+    public void makeDeposit(BigDecimal amount) {
+        throw new RuntimeException("Client not found");
+    }
+
+}
