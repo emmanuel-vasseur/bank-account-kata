67a3544e026917574ba27f355a0933f0aab4b9f7
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2022-01-27 15:18:37 +0100

    feat(operation-history): create Operation - loop 2 - failing test

--- src/test/java/com/sgbu/test/OperationTest.java
+++ src/test/java/com/sgbu/test/OperationTest.java
@@ -1,11 +1,13 @@
 package com.sgbu.test;
 
+import com.sgbu.OperationType;
 import com.sgbu.account.Operation;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,7 +16,12 @@ class OperationTest {
 
     @Test
     void operation_should_encapsulate_attributes() {
-        Operation operation = new Operation(BigDecimal.ONE);
+        Instant operationDate = Instant.now();
+        Operation operation = new Operation(OperationType.DEPOSIT, BigDecimal.ONE, operationDate, BigDecimal.TEN);
         assertThat(operation.getAmount()).isEqualTo(BigDecimal.ONE);
+        assertThat(operation.getType()).isEqualTo(OperationType.DEPOSIT);
+        assertThat(operation.getDate()).isEqualTo(operationDate);
+        assertThat(operation.getBalance()).isEqualTo(BigDecimal.TEN);
     }
+
 }
